a3e6847a8b6a6054f22bd6eec8be996d1fc3ebb6
Syed Hussain Shah Jillani <144683888+HussainShah821@users.noreply.github.com>
2023-10-09 16:16:32 +0500

    Files added

---
+++ script.js
@@ -0,0 +1,25 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const menuForm = document.getElementById("menu-form");
+    const menuList = document.getElementById("menu-list");
+    const promotionForm = document.getElementById("promotion-form");
+    const promotionList = document.getElementById("promotion-list");
+  
+    menuForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const itemName = document.getElementById("itemName").value;
+      const itemPrice = document.getElementById("itemPrice").value;
+      const menuItem = `<li>${itemName} - $${itemPrice}</li>`;
+      menuList.insertAdjacentHTML("beforeend", menuItem);
+      menuForm.reset();
+    });
+  
+    promotionForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const promotionText = document.getElementById("promotionText").value;
+      const promotionItem = `<li>${promotionText}</li>`;
+      promotionList.insertAdjacentHTML("beforeend", promotionItem);
+      promotionForm.reset();
+    });
+  });
+  
